454d17e3d221d072e2d8290afff84187d441cad1
egorbukatkin <yahorbukatkin@gmail.com>
2022-07-17 19:31:27 +0200

    Strings: task_05 complete

---
+++ python/3_Strings/task_05/main.py
@@ -0,0 +1,17 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(0, len(string)):
+        new_string = string[i:i + len(sub_string)]
+        if sub_string in new_string:
+            count += 1
+
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
+
+
