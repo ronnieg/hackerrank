10e67ca847deea8919cbbb409a09579e8355f604
Yahor Bukatkin <yahorbukatkin@mbp-yahor.home>
2024-07-19 11:39:14 +0200

    [hackerrank]: 19.07.2024

--- python/1_Introduction/python-arithmetic-operators.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(a + b, a - b, a * b, sep="\n")
---
+++ python/README.md
@@ -0,0 +1,6 @@
+# Python
+
+[1. Introduction](1_Introduction/README.md)
+
+[2. Basic Data Types](2_Basic_Data_Types/README.md)
+
